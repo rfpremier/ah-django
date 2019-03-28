e731c846b511dcfd33be0ec2ea44d98da5c4f8ff
bencyn <bensonnjung39@gmail.com>
2019-03-28 09:29:27 +0300

    164857216-story(social-login): adding social oauth urls

--- authors/urls.py
+++ authors/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('authors.apps.authentication.urls')),
-
-    # url(r'^$', core_views.home, name='home'),
-
-    url(r'^login/$', auth_views.LoginView, name='login'),
-    url(r'^logout/$', auth_views.LogoutView, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
-    url(r'^admin/', admin.site.urls),
 ]
