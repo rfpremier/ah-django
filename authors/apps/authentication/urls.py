119488c0557d3c6e2686e15670de683bd368f701
bencyn <bensonnjung39@gmail.com>
2019-03-28 09:23:41 +0300

    164857216-story(social-login): update file

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
     url(r'^users/login/?$', LoginAPIView.as_view()),
+
 ]
