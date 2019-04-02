a22494ca15877122ba41711877ddc981bc91f7a8
bencyn <bensonnjung39@gmail.com>
2019-04-03 02:01:55 +0300

    164857216-story(social-login): refactor code

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -63,36 +63,3 @@ class JWTAuthentication(authentication.BaseAuthentication):
         except jwt.ExpiredSignature:
             return False
         return True
-
-
-class JWTokens(object):
-    """
-    Handle token generation
-
-    """
-
-    def create_token(self, user):
-        """
-        Create and return the user's JWT token
-
-        """
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-        payload = jwt_payload_handler(user)
-
-        token = jwt_encode_handler(payload)
-
-        return token
-
-    def encode_token(self, user):
-        """
-        This method gerate token by encoding registered user
-        email address
-        """
-        secret_key = settings.SECRET_KEY
-        payload = {
-            'id': user,
-            'iat': datetime.utcnow()
-        }
-        return jwt.encode(payload, secret_key).decode('UTF-8')
