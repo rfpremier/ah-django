aeb5665a5b9e9c044209fc2862ae9a2cdf9141f9
misocho <misochobria@gmail.com>
2019-04-02 23:28:09 +0300

    * 164857218-feature(article):
    - creates the get one endpoint
    - adds authentication to the post article endpoint
    
    [#164857218]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,23 +1,41 @@
 from rest_framework.mixins import CreateModelMixin
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from django.shortcuts import get_object_or_404
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Articles
 from ..authentication.models import User
 from .serializers import ArticlesSerializer
 
 
-class ArticleView(CreateAPIView, ListAPIView):
+class CreateArticleView(CreateAPIView, ListAPIView):
     queryset = Articles.objects.all()
-
     serializer_class = ArticlesSerializer
 
-    # def perform_create(self, serializer):
-    #     author = get_object_or_404(User, username=self.request.data.get('author'))
-    #     return serializer.save(author=author)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    def post(self, request):
+        if request.user.is_authenticated:
+            article = request.data.get('article', {})
+            author = {"author": request.user.id}
+            article.update(author)
+            serializer = self.serializer_class(
+                data=article
+            )
+            serializer.is_valid(raise_exception=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {
+                    "status": 403,
+                    "error": "Please login"
+                }, status=status.HTTP_403_FORBIDDEN
+            )
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class SingleArticleView(RetrieveAPIView):
+    lookup_field = 'id'
+    queryset = Articles.objects.all()
+    serializer_class = ArticlesSerializer
